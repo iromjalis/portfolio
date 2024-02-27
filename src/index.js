411f9d3f1eeef6bfcca3065f339cf25783788c68
Irina Romjalis <iromjalis@gmail.com>
2024-02-28 00:03:50 +0200

    change tab icon for project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.js";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "@fontsource/outfit";
+import "@fontsource/roboto";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
