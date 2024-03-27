472f12a023a8381a772c500e076633fcdb0e3c31
Irina Romjalis <iromjalis@gmail.com>
2024-03-28 01:51:57 +0200

    className for App container is changing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import styles from "./App.module.css";
-import { Navbar } from "./components/Navbar.jsx";
+import { Navbar } from "./components/Navbar/Navbar.jsx";
+import { Hero } from "./components/Hero/Hero.jsx";
 
 function App() {
   return (
-    <div className={styles.App}>
+    <div className={styles.App_container}>
       <Navbar />
+      <Hero />
     </div>
   );
 }
