1be74e86637605d53b873340560e79439311ce88
Irina Romjalis <iromjalis@gmail.com>
2024-03-28 01:53:13 +0200

    Navbar section with styles is ready

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import styles from "./Navbar.module.css";
+import menuIcon from "../../assets/nav/menuIcon.png";
+import closeIcon from "../../assets/nav/closeIcon.png";
+
+export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <nav className={styles.navbar}>
+      <a href="/" className={styles.title}>
+        Portfolio
+      </a>
+      <div className={styles.menu}>
+        <img
+          className={styles.menuBtn}
+          src={menuOpen ? closeIcon : menuIcon}
+          alt="menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        <ul
+          className={`${styles.menu_list} ${menuOpen && styles.menuOpen}`}
+          onClick={() => setMenuOpen(false)}
+        >
+          <li className={styles.menu_item}>
+            <a
+              href="#about"
+              rel="noopener noreferrer"
+              className={styles.menu_item}
+            >
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              href="#experience"
+              rel="noopener noreferrer"
+              className={styles.menu_item}
+            >
+              Experience
+            </a>
+          </li>
+          <li>
+            <a
+              href="#projects"
+              rel="noopener noreferrer"
+              className={styles.menu_item}
+            >
+              Projects
+            </a>
+          </li>
+          <li>
+            <a
+              href="#contact"
+              rel="noopener noreferrer"
+              className={styles.menu_item}
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
