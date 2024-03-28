a8480aa44ea93d9f50a4472e4064c46fdbaf82c9
Irina Romjalis <iromjalis@gmail.com>
2024-03-29 00:50:06 +0200

    Hero component with styles for mobiles

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+//rafc
+import heroImage from "../../assets/hero/heroImage.png";
+import styles from "./Hero.module.css";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I`m Irina</h1>
+        <p className={styles.description}>
+          I am a full-stack developer, who loves to write code using React and
+          NodeJS. Reach out, if you like to know more!
+        </p>
+        <a
+          className={styles.contactBtn}
+          href="mailto:romjalis.irina@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contact me
+        </a>
+        <img src={heroImage} alt="hero image" className={styles.heroImg} />
+        <div className={styles.topBlur} />
+        <div className={styles.bottomBlur} />
+      </div>
+    </section>
+  );
+};
