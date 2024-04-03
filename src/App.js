c5e702b15d3b3c476520387a02e1b0d7b2239020
Irina Romjalis <iromjalis@gmail.com>
2024-04-04 00:13:44 +0300

    add experience section to App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import styles from "./App.module.css";
 import { Navbar } from "./components/Navbar/Navbar.jsx";
 import { Hero } from "./components/Hero/Hero.jsx";
 import { About } from "./components/About/About.jsx";
+import { Experience } from "./components/Experience/Experience.jsx";
 
 function App() {
   return (
-    <div className={styles.App_container}>
+    <div className={styles.App_container} id="style-1">
       <Navbar />
       <Hero />
       <About />
+      <Experience />
     </div>
   );
 }
