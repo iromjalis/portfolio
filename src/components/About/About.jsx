be021471d4724b8d6db5f3d1cbcb86db6688d002
Irina Romjalis <iromjalis@gmail.com>
2024-04-08 02:10:21 +0300

    added icons for section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,41 +1,60 @@
 import React from "react";
 import styles from "./About.module.css";
 import about from "../../assets/about/aboutImage.png";
+import curcor from "../../assets/about/cursorIcon.png";
+import server from "../../assets/about/serverIcon.png";
+import ui from "../../assets/about/uiIcon.png";
 
 export const About = () => {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.about_title}>About</h1>
-      <img src={about} alt="girl sitting with laptop" />
-      <ul className={styles.about_list}>
-        <li className={styles.about_item}>
-          <a href="/" className={styles.about_link}>
-            Frontend Developer
-          </a>
-          <p className={styles.about_text}>
-            I`m a front-end developer with experience in building responsive and
-            optimized sites
-          </p>
-        </li>
-        <li className={styles.about_item}>
-          <a href="/" className={styles.about_link}>
-            Backend Developer
-          </a>
-          <p className={styles.about_text}>
-            I have experience developing fast and optimised back-end systems and
-            APIs
-          </p>
-        </li>
-        <li className={styles.about_item}>
-          <a href="/" className={styles.about_link}>
-            UI Designer
-          </a>
-          <p className={styles.about_text}>
-            I have designed multiple landing pages and have created design
-            systems as well
-          </p>
-        </li>
-      </ul>
-    </div>
+    <section className={styles.container} id="about">
+      <h2 className={styles.about_title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={about}
+          alt="girl sitting with laptop"
+          className={styles.about_img}
+        />
+
+        <ul className={styles.about_list}>
+          <li className={styles.about_item}>
+            <img src={curcor} alt="cursor icon" />
+
+            <div className={styles.about_text}>
+              <h3 className={styles.about_itemtitle}> Frontend Developer</h3>
+              <p className={styles.about_paragraph}>
+                I`m a front-end developer with experience in building responsive
+                and optimized sites
+              </p>
+            </div>
+          </li>
+
+          <li className={styles.about_item}>
+            <img src={server} alt="server icon" />
+
+            <div className={styles.about_text}>
+              <h3>Backend Developer</h3>
+
+              <p className={styles.about_paragraph}>
+                I have experience developing fast and optimised back-end systems
+                and APIs
+              </p>
+            </div>
+          </li>
+
+          <li className={styles.about_item}>
+            <img src={ui} alt="ui icon" />
+
+            <div className={styles.about_text}>
+              <h3>UI Designer</h3>
+              <p className={styles.about_paragraph}>
+                I have designed multiple landing pages and have created design
+                systems as well
+              </p>{" "}
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
   );
 };
